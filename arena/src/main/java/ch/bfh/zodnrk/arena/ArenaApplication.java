2dcc665534b3ffb6ee14da916a09ba92ed2219b7
Marius Schär <contact@mariusschaer.ch>
2020-11-29 20:44:26 +0100

    Add Arena-App

---
+++ arena/src/main/java/ch/bfh/zodnrk/arena/ArenaApplication.java
@@ -0,0 +1,11 @@
+package ch.bfh.zodnrk.arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArenaApplication.class, args);
+    }
+}
---
+++ arena/src/main/java/ch/bfh/zodnrk/arena/ArenaApplicationRunner.java
@@ -0,0 +1,12 @@
+package ch.bfh.zodnrk.arena;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArenaApplicationRunner implements org.springframework.boot.ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+    }
+}
